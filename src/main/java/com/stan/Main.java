8f084f20642f85b54d0dc18bb82ca6423fa5f239
Stan Choi <sjc591@nyu.edu>
2026-01-10 09:42:04 -0500

    Add naive display menu and main logic

--- src/main/java/com/stan/Main.java
+++ src/main/java/com/stan/Main.java
@@ -3,9 +3,53 @@ package com.stan;
 // TODO 2. create a package with your name. i.e com.franco and move this file inside the new package
 // TODO 3. implement https://amigoscode.com/learn/java-cli-build/lectures/3a83ecf3-e837-4ae5-85a8-f8ae3f60f7f5
 
-public class Main {
+import com.stan.car.Car;
+import com.stan.car.CarDao;
+import com.stan.car.CarService;
 
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Master Class");
+        CarDao carDao = new CarDao();
+        CarService carService = new CarService(carDao);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println();
+                displayMenu();
+                System.out.println();
+                String userInput = scanner.nextLine();
+                try {
+                    int userInputNumber = Integer.parseInt(userInput);
+                    if (userInputNumber < 1 || userInputNumber > 7) {
+                        continue;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid user input: " + userInput);
+                    e.printStackTrace();
+                }
+                switch (userInput) {
+                    case "4":
+                        Car[] cars = carService.getCars();
+                        // probably can handle better with DTOs
+                        for (Car car : cars) {
+                            System.out.println(car);
+                        }
+                        break;
+                    case "7":
+                        return;
+                }
+            }
+        }
+    }
+
+    public static void displayMenu() {
+        System.out.println("1️⃣ - Book Car");
+        System.out.println("2️⃣ - View All User Booked Cars");
+        System.out.println("3️⃣ - View All Bookings");
+        System.out.println("4️⃣ - View Available Cars");
+        System.out.println("5️⃣ - View Available Electric Cars");
+        System.out.println("6️⃣ - View all users");
+        System.out.println("7️⃣ - Exit");
     }
 }
